f47c90dd54858ca5fca163d84c21cace38fee3be
bensonAng03 <117569632+bensonAng03@users.noreply.github.com>
2023-09-14 09:19:25 +0800

    version 1.1.23

--- src/components/LiveCourse/Chatroom/Chatroom.jsx
+++ src/components/LiveCourse/Chatroom/Chatroom.jsx
@@ -1,48 +1,52 @@
 import { useEffect, useState, forwardRef, useRef } from "react";
 import classes from "./Chatroom.module.css";
+
 const Chatroom = forwardRef(function Chatroom(
   { messages, handleSendMessage, name },
   ref
 ) {
-  const [isMouseEnter,setIsMouseEnter]=useState(false)
+  const [isMouseEnter, setIsMouseEnter] = useState(false);
   const [canSendMessage, setCanSendMessage] = useState(true);
-  const [sendMessage,setSendMessage]=useState(false)
+  const [sendMessage, setSendMessage] = useState(false);
   const inputRef = useRef(null);
   let timer = null;
+
   useEffect(() => {
     if (isMouseEnter && sendMessage) {
-      scrollToBottom()
-      setSendMessage(false)
-    }else if(!isMouseEnter){
-      scrollToBottom()
+      scrollToBottom();
+      setSendMessage(false);
+    } else if (!isMouseEnter) {
+      scrollToBottom();
     }
-  }, [messages, isMouseEnter,sendMessage]);
-  const scrollToBottom=()=>{
+  }, [messages, isMouseEnter, sendMessage]);
+
+  const scrollToBottom = () => {
     if (ref.current) {
       ref.current.scrollTop = ref.current.scrollHeight;
     }
-  }
+  };
+
   const handleSendMessageFn = () => {
     if (canSendMessage) {
       handleSendMessage(inputRef.current.value);
       inputRef.current.value = "";
       setCanSendMessage(false);
-      setSendMessage(true)
+      setSendMessage(true);
       clearTimeout(timer);
       timer = setTimeout(() => {
         setCanSendMessage(true);
       }, 1000);
     }
   };
+
   return (
     <div
-    id="chatroom"
+      id="chatroom"
       className={classes.Chatroom}
       onMouseEnter={() => setIsMouseEnter(true)}
-    
       onMouseLeave={() => setIsMouseEnter(false)}
     >
-      <h3>聊天室</h3>
+      <h3>Chatroom</h3>
       <div className={classes.Messages} ref={ref}>
         {messages.map((message, index) => (
           <div key={index}>
@@ -73,11 +77,7 @@ const Chatroom = forwardRef(function Chatroom(
         ))}
       </div>
       <div className={classes.InputContainer}>
-        <input
-          ref={inputRef}
-          type="text"
-          placeholder="输入"
-        />
+        <input ref={inputRef} type="text" placeholder="Input" />
         <button
           className={
             canSendMessage
@@ -86,12 +86,12 @@ const Chatroom = forwardRef(function Chatroom(
           }
           onClick={handleSendMessageFn}
         >
-          发送消息
+          Send Message
         </button>
       </div>
-      {isMouseEnter && ref.current.scrollHeight>260 && (
+      {isMouseEnter && ref.current.scrollHeight > 260 && (
         <button className={classes.ScrollButton} onClick={scrollToBottom}>
-          滚动到底部
+          Scroll to Bottom
         </button>
       )}
     </div>
